8e3c65ba0801f2e1b00ecbee05024de0d0d72eab
Juan <malagasulca@gmail.com>
2021-04-17 10:28:09 -0500

    Usuario

--- OdontoSis/prueba2.py
+++ OdontoSis/prueba2.py
@@ -1,28 +1,86 @@
-import tkinter as tk
-from PIL import ImageTk, Image
-import ctypes
+from tkinter import *						
+import tkinter as tk	
+from PIL import ImageTk,Image			#para manipular imagenes
+from tkinter import messagebox as mb	#Nos permite abrir ventanas de mensajes
+import sqlite3							#Nos permite conectarnos a una base de datos (sqlite3)
 
-resolucion = ctypes.windll.user32 
+ventana=tk.Tk()
+ventana.title("_Mi primer Login_")	#Titulo de la ventana principal
+ventana.geometry("280x450+300+250")	#Tamaño de nuestra ventana Principal
+	
+color='#c5e2f6'			#Codigo HEX del color de fondo usado
+ventana['bg']=color		#Definimos nuestra ventana 'bg' con el valor 'color'
 
-root = tk.Tk()
+Label(ventana,bg=color,text="Login",font=("Arial Black",16)).pack()	#Mostramos texto 'Login'
 
-img = Image.open("./src/caratula.png")
+#Cajas de nuestra ventana Principal
+Label(ventana,text="Usuario : ",bg=color,font=("Arial Black",10)).pack()	#Texto 'Usuario:'
+caja1=Entry(ventana,font=("Arial",10))										#Creamos una caja de texto 'caja1'
+caja1.pack()																#Posicion de la 'caja1'
+Label(ventana,text="Contraseña : ",bg=color,font=("Arial Black",10)).pack()	#Texto 'Contraseña:'
+caja2=Entry(ventana,show="*")												#Creamos la 'caja2' (contraseña)
+caja2.pack()																#Posicion de 'caja2'
 
-w = resolucion.GetSystemMetrics(0)
-h = resolucion.GetSystemMetrics(1)
+db=sqlite3.connect('approved.db')		#Nos conectamos a nuestra base de datos 'login.db'
+c=db.cursor()						#Establecemos un cursor
 
-o_size = img.size   #Tamaño original de la imagen
-f_size = (w/4, h/4) #Tamaño del canvas donde se mostrará la imagen
+def login():				#Funcion login ... Nos permitira comprobar 'usuario' y 'contraseña' con la base de datos
+	usuario=caja1.get()		#Obtenemos el valor de la 'caja1' (usuario)
+	contr=caja2.get()		#Obtenemos el valor de la 'caja2' (contraseña)
+	c.execute('SELECT * FROM usuarios WHERE Usuario = ? AND Pass = ?',(usuario,contr))	#Seleccionamos datos '(usuario,contr)'
+	if c.fetchall():
+		mb.showinfo(title="Login Correcto",message="Usuario y contraseña correctos")		#Mostramos 'Login Correcto'
+	else:
+		mb.showerror(title="Login incorrecto",message="Usuario o contraseña incorrecto")	#Mostramos 'Login incorrecto'
+	#c.close()
 
-factor = min(float(f_size[1])/o_size[1], float(f_size[0])/o_size[0])
-width = int(o_size[0] * factor)
-height = int(o_size[1] * factor)
+def nuevaVentana():							#Funcion nuevaVentana ... Nos permitira el registro de nuevos usuarios
+	newVentana=tk.Toplevel(ventana)			#Definimos 'newVentana'
+	newVentana.title("Registro de Usuario")	#Le damos el titulo 'Registro de Usuario'
+	newVentana.geometry("300x290+800+250")	#Tamaño de la ventana
+	newVentana['bg']=color					#Definimos newVentana 'bg' con el valor de 'color'
+	
+	labelExample=tk.Label(newVentana,text="Registro : ",bg=color,font=("Arial Black",12)).pack(side="top")	#Texto 'Registro'
 
-rImg= img.resize((width, height), Image.ANTIALIAS)
-rImg = ImageTk.PhotoImage(rImg)
+	Label(newVentana,text="Nombre : ",bg=color,font=("Arial Black",10)).pack()		#Texto 'Nombre:'
+	caja3=Entry(newVentana)															#Creamos 'caja3' (Nombre)
+	caja3.pack()
+	Label(newVentana,text="Apellidos : ",bg=color,font=("Arial Black",10)).pack()	#Texto 'Apellidos'
+	caja4=Entry(newVentana)															#Creamos 'caja4' (Apellidos)
+	caja4.pack()
+	Label(newVentana,text="Usuario : ",bg=color,font=("Arial Black",10)).pack()		#Texto 'Usuario'
+	caja5=Entry(newVentana)															#Creamos 'caja5' (Usuario)
+	caja5.pack()
+	Label(newVentana,text="Contraseña : ",bg=color,font=("Arial Black",10)).pack()	#Texto 'Contraseña'
+	caja6=Entry(newVentana,show="*")												#Creamos 'caja6' (Contraseña)
+	caja6.pack()	
+	Label(newVentana,text="Repita la Contraseña : ",bg=color,font=("Arial Black",10)).pack()	#Texto 'Repita la Contraseña'
+	caja7=Entry(newVentana,show="*")															#Creamos 'caja7' 
+	caja7.pack()
+	def registro():				#Funcion registro ... Nos permitira escribir los datos a nuestra base de datos
+		Nombre=caja3.get()		#Obtenemos el valor de 'caja3'
+		Apellido=caja4.get()	#Obtenemos el valor de 'caja4'
+		Usr_reg=caja5.get()		#Obtenemos el valor de 'caja5'
+		Contra_reg=caja6.get()	#Obtenemos el valor de 'caja6'
+		Contra_reg_2=caja7.get() #Obtenemos el valor de 'caja7'
+		if(Contra_reg==Contra_reg_2):		#Esta condicion nos permite saber si las contraseñas coinciden
+			#El siguiente comando es el encargado de insertar los datos obtenidos en el registro
+			c.execute("INSERT INTO usuarios values(\'"+Nombre+"\',\'"+Apellido+"\',\'"+Usr_reg+"\',\'"+Contra_reg+"')")
+			db.commit()			#Confirmamos los datos
+			mb.showinfo(title="Registro Correcto",message="Hola "+Nombre+" "+Apellido+" ¡¡ \nSu registro fue exitoso.")
+			newVentana.destroy()		#Cerramos la ventana de registro
+		else:	#Se ejecutara si las contraseñas no coinciden
+			mb.showerror(title="Contraseña Incorrecta",message="Error¡¡¡ \nLas contraseñas no coinciden.")	#Mostramos un mensaje
+		#c.close()		#Nos permite cerrar el cursor ...
+		#db.close()
+	#El siguiente comando (boton) nos permite llamar ala funcion registro
+	buttons=tk.Button(newVentana,text="Registrar ¡",command=registro,bg=color,font=("Arial Rounded MT Bold",10)).pack(side="bottom")
+	
+Label(ventana,text=" ",bg=color,font=("Arial",10)).pack()		#Solo es una linea vacia ... (lo use para separar el boton) 
+Button(text=" ENTRAR ",command=login,bg='#a6d4f2',font=("Arial Rounded MT Bold",10)).pack()		#Boton ==> funcion 'login'
+Label(ventana,text=" ",bg=color,font=("Arial Black",10)).pack()
+Label(ventana,text="No tienes una cuenta ? : ",bg=color,font=("Arial Black",10)).pack()		#Simple texto
+#La siguiente linea (boton) nos llama ala funcion 'nuevaVentana' ==> ( ventana de registro)
+boton1=Button(ventana,text="REGISTRO",bg='#a6d4f2',command=nuevaVentana,font=("Arial Rounded MT Bold",10)).pack()
 
-canvas = tk.Canvas(root, width=f_size[0], height= f_size[1])
-canvas.create_image(f_size[0]/2, f_size[1]/2, anchor=tk.CENTER, image=rImg, tags="img")
-canvas.pack(fill=None, expand=False)
-
-root.mainloop()
+ventana.mainloop()
